2b953158d6cfbc48d454b1201b2992b84a69ce41
Aleksandr Ivanov <eealeivan@gmail.com>
2013-10-01 23:11:17 +0300

    Changed config structure.

--- src/Mixpanel/Mixpanel/MixpanelConfig.cs
+++ src/Mixpanel/Mixpanel/MixpanelConfig.cs
@@ -2,10 +2,29 @@
 
 namespace Mixpanel
 {
-    public static class MixpanelConfig
+    public static class MixpanelGlobalConfig
     {
-        public static Func<object, string> JsonSerializer { get; set; }
-        public static bool ConvertPropertyNamesToSentenceCase { get; set; }
-        public static bool ConvertPropertyNamesToTitleCase { get; set; }
+        public static Func<object, string> SerializeJsonFn { get; set; }
+        public static PropertyNameFormat PropertyNameFormat { get; set; }
+
+        public static void Reset()
+        {
+            SerializeJsonFn = null;
+            PropertyNameFormat = PropertyNameFormat.None;
+        }
+    }
+
+    public class MixpanelConfig
+    {
+        public Func<object, string> SerializeJsonFn { get; set; }
+        public PropertyNameFormat PropertyNameFormat { get; set; }
+    }
+
+    public enum PropertyNameFormat
+    {
+        None,
+        SentenceTitleCase,
+        SentenseCapitilized,
+        SentenceLowerCase
     }
 }
