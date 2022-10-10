68f73ed3a4e16981c6e070d99dcd23be094eecf9
Aleksandr Ivanov <eealeivan@gmail.com>
2022-10-10 21:42:49 +0300

    Start adding the support for querying Mixpanel API.

---
+++ src/Mixpanel/Mixpanel.Tests.Integration/MixpanelClientTrackTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bogus;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Mixpanel.Tests.Integration
+{
+    public class MixpanelClientTrackTests
+    {
+        [Test]
+        public async Task TrackAsync_EventOnly_MessageSent()
+        {
+            // Arrange
+            var (token, _, _, @event) = GenerateInputs();
+            var client = new MixpanelClient(token);
+
+            // Act
+            var result = await client.TrackAsync(@event, new {});
+ 
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task TrackAsync_SimpleMessage_CorrectDataSavedInMixpanel()
+        {
+            // Arrange
+            var (token, _, insertId, @event) = GenerateInputs();
+            var client = new MixpanelClient(token);
+
+            // Act
+            await client.TrackAsync(@event, new Dictionary<string, object>
+            {
+                { "$insert_id", insertId },
+                //{ MixpanelProperty.DistinctId, "john.doe@gmail.com" },
+                //{ "app_version", "1.1.34.694.gac68a2b3" },
+                //{ "song_id", "0wwPcA6wtMf6HUMpIRdeP7" },
+            });
+
+            // Assert
+            var json = await MixpanelApi.GetRecentEvent(SecretsManager.MixpanelProjectId, insertId);
+        }
+
+        private (string token, string distinctId, string insertId, string @event) GenerateInputs()
+        {
+            var randomizer = new Randomizer();
+            return
+            (
+                SecretsManager.MixpanelProjectToken,
+                randomizer.AlphaNumeric(10),
+                randomizer.AlphaNumeric(20),
+                randomizer.Words()
+            );
+        }
+    }
+}
---
+++ src/Mixpanel/Mixpanel.Tests.Integration/SecretsManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Mixpanel.Tests.Integration
+{
+    public static class SecretsManager
+    {
+        public static string MixpanelProjectId { get; }
+        public static string MixpanelProjectToken { get; }
+        public static string MixpanelServiceAccountUsername { get; }
+        public static string MixpanelServiceAccountSecret { get; }
+
+        static SecretsManager()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<DefaultHttpClientTests>();
+
+            IConfiguration configuration = builder.Build();
+
+            MixpanelProjectId = configuration["Mixpanel:Project:Id"];
+            if (string.IsNullOrWhiteSpace(MixpanelProjectId))
+            {
+                throw new Exception("Mixpanel:ProjectId is empty.");
+            }
+
+            MixpanelProjectToken = configuration["Mixpanel:Project:Token"];
+            if (string.IsNullOrWhiteSpace(MixpanelProjectToken))
+            {
+                throw new Exception("Mixpanel:ProjectToken is empty.");
+            }
+
+            MixpanelServiceAccountUsername = configuration["Mixpanel:ServiceAccount:Username"];
+            if (string.IsNullOrWhiteSpace(MixpanelServiceAccountUsername))
+            {
+                throw new Exception("Mixpanel:ServiceAccount:Username is empty.");
+            }
+
+            MixpanelServiceAccountSecret = configuration["Mixpanel:ServiceAccount:Secret"];
+            if (string.IsNullOrWhiteSpace(MixpanelServiceAccountSecret))
+            {
+                throw new Exception("Mixpanel:ServiceAccount:Secret is empty.");
+            }
+        }
+    }
+}
